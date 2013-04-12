e6ddc6cfd92a3c45773f8cd67b575ad0b3ced4e8
David Kitchen <david@buro9.com>
2013-04-12 18:17:11 +0100

    Added docs on /comments

--- examples/comments/id/put.sh
+++ examples/comments/id/put.sh
@@ -1,15 +1,8 @@
-# New comment on item
 curl -i \
 	-X PUT \
 	-H "Authorization: Bearer letmein" \
 	-H "Content-Type: application/json" \
-	-d '{"itemType": "conversation","itemId": 1,"markdown": "An Updated First Level Header\n====================\n\nA Second Level Header\n---------------------\n\nNow is the time for all good men (such as +awesome_guy +blueprints) to\ncome to the aid of their community: http://www.lfgss.com/ . This is just a\nregular paragraph.\n\nThe quick brown [fox](http://www.google.com/?q=fox) jumped over the lazy\ndogs back.\n\n### Header 3\n\n> This is a blockquote.\n> \n> This is the second paragraph in the blockquote.\n>\n> ## This is an H2 +Velocio @blueprints are mentioned in a blockquote"}' \
+	-d '{"itemType": "conversation","itemId": 1,"markdown": "Some text and a link to [Yahoo](http://www.yahoo.com/)"}' \
 	http://esi01.dev.microcosm.cc/api/v1/comments
 
-{
-	"itemType": "conversation",
-	"itemId": 1,
-	"markdown": "An Updated First Level Header\n====================\n\nA Second Level Header\n---------------------\n\nNow is the time for all good men (such as +awesome_guy +blueprints) to\ncome to the aid of their community: http://www.lfgss.com/ . This is just a\nregular paragraph.\n\nThe quick brown [fox](http://www.google.com/?q=fox) jumped over the lazy\ndogs back.\n\n### Header 3\n\n> This is a blockquote.\n> \n> This is the second paragraph in the blockquote.\n>\n> ## This is an H2 +Velocio @blueprints are mentioned in a blockquote"
-}
-
-# Update a comment in reply to existing comment, on item is identical to updating a normal comment. inReplyTo is ignored on PUT
+# Update a comment in reply to existing comment is identical to updating a normal comment. inReplyTo is ignored on PUT
