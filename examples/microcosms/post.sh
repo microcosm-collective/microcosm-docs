be56583882d4b4fddfbd17676d233e1301441750
David Kitchen <david@buro9.com>
2013-11-21 14:56:35 +0000

    Updated samples for site and microcosms

--- examples/microcosms/post.sh
+++ examples/microcosms/post.sh
@@ -2,5 +2,5 @@ curl -i \
 	-X POST \
 	-H "Authorization: Bearer letmein" \
 	-H "Content-Type: application/json" \
-	-d '{"visibility":"public", "title":"General Chit-Chat","description":"A place to talk about anything and everything."}' \
+	-d '{"title":"Rivendell", "description":"Refuge of the Elves, the House of Elrond lies within."}' \
 	https://dev1.microco.sm/api/v1/microcosms
