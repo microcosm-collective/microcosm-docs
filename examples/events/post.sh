fe3bb29e3a951f153a9c7359b599813792555a2c
David Kitchen <david@buro9.com>
2013-11-21 17:35:52 +0000

    Documented event creation

--- examples/events/post.sh
+++ examples/events/post.sh
@@ -1,16 +1,16 @@
 # Proposed event
 curl -i \
-	-X POST \
-	-H "Authorization: Bearer letmein" \
-	-H "Content-Type: application/json" \
-	-d '{"microcosmId": 1,"title": "Introduction to Track Riding"}' \
+  -X POST \
+  -H "Authorization: Bearer letmein" \
+  -H "Content-Type: application/json" \
+  -d '{"microcosmId": 1,"title": "Ale tasting?"}' \
 	https://dev1.microco.sm/api/v1/events
 
 # Upcoming event
 curl -i \
-	-X POST \
-	-H "Authorization: Bearer letmein" \
-	-H "Content-Type: application/json" \
-	-d '{"microcosmId": 1,"title": "Introduction to Track Riding","when": "2013-09-15T09:00:00Z","duration": 180,"where": "Herne Hill Velodrome, SE24 9HE","rsvpLimit": 50}' \
-	https://dev1.microco.sm/api/v1/events
+  -X POST \
+  -H "Authorization: Bearer letmein" \
+  -H "Content-Type: application/json" \
+  -d '{"microcosmId": 1, "title": "Ale tasting", "when": "2013-11-22T20:00:00Z", "duration": 180, "where": "Green Dragon Inn", "lat": 51.674871, "lon": -0.216352, "north": 51.679581, "east": -0.208955, "south": 51.66984, "west": -0.227494, "rsvpLimit": 5}' \
+  https://dev1.microco.sm/api/v1/events
 	
