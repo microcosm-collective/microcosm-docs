15e595be4df0b8cc75f770a001eb13753a740dea
David Kitchen <david@buro9.com>
2013-11-21 15:24:55 +0000

    Updated the microcosm documentation

--- examples/microcosms/id/put.sh
+++ examples/microcosms/id/put.sh
@@ -2,5 +2,5 @@ curl -i \
 	-X PUT \
 	-H "Authorization: Bearer letmein" \
 	-H "Content-Type: application/json" \
-	-d '{"visibility": "public","title": "General Chit-Chat","description": "A place to talk about anything and everything.","meta":{"editReason": "Updated the title"}}' \
+	-d '{"title": "Rivendell","description": "Refuge of the Elves, also known as Imladris. The House of Elrond is within Rivendell.","meta":{"editReason": "Updated the description"}}' \
 	https://dev1.microco.sm/api/v1/microcosms/1
