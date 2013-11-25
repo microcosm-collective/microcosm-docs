a57968b3bc163bf71b69ddf45b817d1cc94c0db9
David Kitchen <david@buro9.com>
2013-11-25 16:33:01 +0000

    Updated Documentation on files and attachments

---
+++ examples/attachments/id/delete.sh
@@ -0,0 +1,4 @@
+curl -i \
+  -X DELETE \
+  -H "Authorization: Bearer letmein" \
+  https://dev1.microco.sm/api/v1/comments/1/attachments/6dfa7765c0426db8d27fda63fa9671794dd730a3
---
+++ examples/attachments/post.sh
@@ -0,0 +1,6 @@
+curl -i \
+  -X POST \
+  -H "Authorization: Bearer letmein" \
+  -H "Content-Type: application/json" \
+  -d '{"FileHash":"6dfa7765c0426db8d27fda63fa9671794dd730a3"}' \
+  https://dev1.microco.sm/api/v1/comments/1/attachments
