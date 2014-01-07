7b2cad7fb4b4ba0e15881e2e29171d882e571fd2
David Kitchen <david@buro9.com>
2014-01-07 13:43:14 +0000

    Updated attendees documentation for mass creation of attendees

--- examples/events/id/attendees/put.sh
+++ examples/events/id/attendees/put.sh
@@ -1,15 +1,15 @@
 # Invite/Tentative
 curl -i \
-	-X POST \
+	-X PUT \
 	-H "Authorization: Bearer letmein" \
 	-H "Content-Type: application/json" \
-	-d '{"attendeeId": 1}' \
+	-d '[{"attendeeId": 1}]' \
 	https://dev1.microco.sm/api/v1/events/1/attendees
 
 # User Confirming
 curl -i \
-	-X POST \
+	-X PUT \
 	-H "Authorization: Bearer letmein" \
 	-H "Content-Type: application/json" \
-	-d '{"attendeeId": 1, "rsvp":"yes"}' \
+	-d '[{"attendeeId": 1, "rsvp":"yes"}]' \
 	https://dev1.microco.sm/api/v1/events/1/attendees
