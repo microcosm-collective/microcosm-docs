209444235b2cba276e08cd1538986c36d8776bee
cyclotron3k <aidan.samuel@gmail.com>
2025-01-27 23:30:49 +1100

    Update

--- examples/attachments/post.sh
+++ examples/attachments/post.sh
@@ -2,5 +2,5 @@ curl -i \
   -X POST \
   -H "Authorization: Bearer letmein" \
   -H "Content-Type: application/json" \
-  -d '{"FileHash":"6dfa7765c0426db8d27fda63fa9671794dd730a3"}' \
+  -d '{"FileHash":"6dfa7765c0426db8d27fda63fa9671794dd730a3", "FileName":"image.jpeg"}' \
   https://dev1.microco.sm/api/v1/comments/1/attachments
--- examples/events/post.sh
+++ examples/events/post.sh
@@ -11,6 +11,6 @@ curl -i \
   -X POST \
   -H "Authorization: Bearer letmein" \
   -H "Content-Type: application/json" \
-  -d '{"microcosmId": 1, "title": "Ale tasting", "when": "2013-11-22T20:00:00Z", "duration": 180, "where": "Green Dragon Inn", "lat": 51.674871, "lon": -0.216352, "north": 51.679581, "east": -0.208955, "south": 51.66984, "west": -0.227494, "rsvpLimit": 5}' \
+  -d '{"microcosmId": 1, "title": "Ale tasting", "when": "2013-11-22T20:00:00Z", "tz": "Europe/London", duration": 180, "where": "Green Dragon Inn", "lat": 51.674871, "lon": -0.216352, "north": 51.679581, "east": -0.208955, "south": 51.66984, "west": -0.227494, "rsvpLimit": 5}' \
   https://dev1.microco.sm/api/v1/events
 	
